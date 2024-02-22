388e1cb30a0d2e88d5e53678c99414683a76a1d3
Shreyaagrawal23 <miss.shreya.miss@gmail.com>
2024-02-22 16:54:53 +0530

    responsive

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function loaderAnimation() {
   tl.from(".line h1", {
     y: 150,
     stagger: 0.25,
-    durantion: 0.6,
+    duration: 0.6,
     delay: 0.5,
   });
   tl.from("#line1-part1", {
@@ -65,13 +65,13 @@ function loaderAnimation() {
   });
   tl.to("#loader", {
     opacity: 0,
-    durantion: 0.4,
+    duration: 0.4,
     delay: 2,
   });
   tl.from("#page1", {
     y: 1600,
     ease: Power4,
-    durantion: 0.6,
+    duration: 0.6,
     delay: 0,
   });
   tl.to("#loader", {
@@ -97,7 +97,7 @@ function cursorAnimation() {
   Shery.mouseFollower({
     skew: true,
     ease: "cubic-bezier(0.23,1,0.320,1)",
-    durantion: 1,
+    duration: 1,
   });
   Shery.makeMagnet("#nav2 h4", {});
   
@@ -153,45 +153,48 @@ if (
   !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     navigator.userAgent
   )
-) {
-function sheryAnimation() {
-  Shery.imageEffect(".img-div", {
-    style: 5,
-    config: {
-      a: { value: 2, range: [0, 30] },
-      b: { value: -0.54, range: [-1, 1] },
-      zindex: { value: -9996999, range: [-9999999, 9999999] },
-      aspect: { value: 0.7272749932567818 },
-      ignoreShapeAspect: { value: true },
-      shapePosition: { value: { x: 0, y: 0 } },
-      shapeScale: { value: { x: 0.5, y: 0.5 } },
-      shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
-      shapeRadius: { value: 0, range: [0, 2] },
-      currentScroll: { value: 0 },
-      scrollLerp: { value: 0.07 },
-      gooey: { value: true },
-      infiniteGooey: { value: false },
-      growSize: { value: 4, range: [1, 15] },
-      durationOut: { value: 1, range: [0.1, 5] },
-      durationIn: { value: 1.5, range: [0.1, 5] },
-      displaceAmount: { value: 0.5 },
-      masker: { value: false },
-      maskVal: { value: 1, range: [1, 5] },
-      scrollType: { value: 0 },
-      geoVertex: { range: [1, 64], value: 1 },
-      noEffectGooey: { value: true },
-      onMouse: { value: 0 },
-      noise_speed: { value: 0.2, range: [0, 10] },
-      metaball: { value: 0.4, range: [0, 2] },
-      discard_threshold: { value: 0.5, range: [0, 1] },
-      antialias_threshold: { value: 0, range: [0, 0.1] },
-      noise_height: { value: 0.5, range: [0, 2] },
-      noise_scale: { value: 10, range: [0, 100] },
-    },
-    gooey: true,
-  });
-}
-}
+) {}
+  function sheryAnimation() {
+    Shery.imageEffect(".img-div", {
+      style: 5,
+      gooey: true,
+      config: {
+        a: { value: 2, range: [0, 30] },
+        b: { value: -0.54, range: [-1, 1] },
+        zindex: { value: -9996999, range: [-9999999, 9999999] },
+        aspect: { value: 0.7272749932567818 },
+        ignoreShapeAspect: { value: true },
+        shapePosition: { value: { x: 0, y: 0 } },
+        shapeScale: { value: { x: 0.5, y: 0.5 } },
+        shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
+        shapeRadius: { value: 0, range: [0, 2] },
+        currentScroll: { value: 0 },
+        scrollLerp: { value: 0.07 },
+        gooey: { value: true },
+        infiniteGooey: { value: false },
+        growSize: { value: 4, range: [1, 15] },
+        durationOut: { value: 1, range: [0.1, 5] },
+        durationIn: { value: 1.5, range: [0.1, 5] },
+        displaceAmount: { value: 0.5 },
+        masker: { value: false },
+        maskVal: { value: 1, range: [1, 5] },
+        scrollType: { value: 0 },
+        geoVertex: { range: [1, 64], value: 1 },
+        noEffectGooey: { value: true },
+        onMouse: { value: 0 },
+        noise_speed: { value: 0.2, range: [0, 10] },
+        metaball: { value: 0.4, range: [0, 2] },
+        discard_threshold: { value: 0.5, range: [0, 1] },
+        antialias_threshold: { value: 0, range: [0, 0.1] },
+        noise_height: { value: 0.5, range: [0, 2] },
+        noise_scale: { value: 10, range: [0, 100] },
+      },
+     
+    });
+  }
+
+
+
 function flag(){
   document.addEventListener("mousemove",function(dets){
     gsap.to("#Flag",{
